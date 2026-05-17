9fd3e96914374677c8d968a4b056acdec6a71cbd
Công Ty TNHH Tư Vấn Thiết Kế Xây Dựng MK <97280663+Minhkhoa2206@users.noreply.github.com>
2026-05-17 10:46:05 +0700

    Update shopee.js

--- api/shopee.js
+++ api/shopee.js
@@ -1,44 +1,9 @@
-export default async function handler(req, res) {
+const response = await fetch(
 
-    const { shopid, itemid } = req.query;
+`/api/shopee?url=${encodeURIComponent(link)}`
 
-    if(!shopid || !itemid){
+);
 
-        return res.status(400).json({
-            error:"Missing params"
-        });
-    }
+const data = await response.json();
 
-    try{
-
-        const response = await fetch(
-
-            `https://shopee.vn/api/v4/item/get?itemid=${itemid}&shopid=${shopid}`,
-
-            {
-                headers:{
-
-                    "User-Agent":
-                    "Mozilla/5.0",
-
-                    "Referer":
-                    "https://shopee.vn/",
-
-                    "Origin":
-                    "https://shopee.vn"
-                }
-            }
-        );
-
-        const data =
-        await response.json();
-
-        res.status(200).json(data);
-
-    }catch(err){
-
-        res.status(500).json({
-            error:err.toString()
-        });
-    }
-}
+console.log(data);
